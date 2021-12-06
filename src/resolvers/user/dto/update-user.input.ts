53a1796bc378e7a5c93bb0b6de22a058eac995f5
Brendon Colby <brendon@brendoncolby.com>
2021-12-06 17:01:23 -0500

    minor tweaks

--- src/resolvers/user/dto/update-user.input.ts
+++ src/resolvers/user/dto/update-user.input.ts
@@ -1,10 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { MaxLength } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput {
   @Field({ nullable: true })
+  @MaxLength(100)
   firstname?: string;
 
   @Field({ nullable: true })
+  @MaxLength(100)
   lastname?: string;
 }
